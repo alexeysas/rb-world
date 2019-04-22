b1935d9166e1b1a81cf270728aac067059d5905a
root <asasryl@gmail.com>
2019-04-22 13:04:46 +0000

    slam

---
+++ slam/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export TURTLEBOT_GAZEBO_MAP_FILE="/home/workspace/rb-world/slam/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/maps/playground.yaml"
+export TURTLEBOT_STAGE_MAP_FILE="/home/workspace/rb-world/slam/catkin_ws/src/turtlebot_simulator/turtlebot_stage/maps/maze.yaml"
+export TURTLEBOT_STAGE_WORLD_FILE="/home/workspace/rb-world/slam/catkin_ws/src/turtlebot_simulator/turtlebot_stage/maps/stage/maze.world"
+export TURTLEBOT_STDR_MAP_FILE="/home/workspace/rb-world/slam/catkin_ws/src/turtlebot_simulator/turtlebot_stdr/maps/sparse_obstacles.yaml"
+
+# modified environment variables
+export PKG_CONFIG_PATH="/home/workspace/rb-world/slam/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/workspace/rb-world/slam/catkin_ws/build"
